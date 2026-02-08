3f70ca036318c265c4aaaf9b67f6162d33f2cf46
thanai20091-netizen <thanai20091@gmail.com>
2026-02-08 11:03:09 +0400

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "file:///C:/Users/Admin/Downloads/Finding%20Her%20Kushagra%20320%20Kbps.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/drsfuakwy/video/upload/v1770534113/Finding_Her_Kushagra_320_Kbps_dx38uy.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
